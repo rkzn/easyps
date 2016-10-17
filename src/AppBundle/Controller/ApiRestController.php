d5e1b488e98b846d36d37cdaacbbef63e4bfd683
Roma Kazansky <roma@kazansky.su>
2016-10-17 21:28:48 +0300

    routing

--- src/AppBundle/Controller/ApiRestController.php
+++ src/AppBundle/Controller/ApiRestController.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * This file is part of the FOSRestByExample package.
- *
- * (c) Santiago Diaz <santiago.diaz@me.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- */
+
 namespace AppBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
